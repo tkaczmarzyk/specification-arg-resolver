4dc30f4bfeca11c196fce84af839b262fc16faa1
Tomasz Kaczmarzyk <tomasz+github@kaczmarzyk.net>
2014-03-23 12:51:40 +0100

    Conjunction specification

---
+++ src/main/java/net/kaczmarzyk/spring/data/jpa/domain/Conjunction.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.kaczmarzyk.spring.data.jpa.domain;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.domain.Specifications;
+
+/**
+ * Helper for easier joining lists of specs with {@code AND} operator
+ * 
+ * @author Tomasz Kaczmarzyk
+ */
+public class Conjunction<T> implements Specification<T> {
+
+    private Specifications<T> combinedSpecs;
+
+    
+    @SafeVarargs
+    public Conjunction(Specification<T>... innerSpecs) {
+        this(Arrays.asList(innerSpecs));
+    }
+    
+    public Conjunction(Collection<Specification<T>> innerSpecs) {
+        for (Specification<T> spec : innerSpecs) {
+            if (combinedSpecs == null) {
+                combinedSpecs = Specifications.where(spec);
+            } else {
+                combinedSpecs = combinedSpecs.and(spec);
+            }
+        }
+    }
+
+    @Override
+    public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+        return combinedSpecs.toPredicate(root, query, cb);
+    }
+
+}
---
+++ src/test/java/net/kaczmarzyk/spring/data/jpa/domain/ConjunctionTest.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.kaczmarzyk.spring.data.jpa.domain;
+
+import static net.kaczmarzyk.spring.data.jpa.CustomerBuilder.customer;
+import static org.assertj.core.api.Assertions.*;
+import java.text.ParseException;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import net.kaczmarzyk.spring.data.jpa.Customer;
+import net.kaczmarzyk.spring.data.jpa.IntegrationTestBase;
+
+
+public class ConjunctionTest extends IntegrationTestBase {
+
+    Customer homerSimpson;
+    Customer margeSimpson;
+    Customer bartSimpson;
+    Customer moeSzyslak;
+    Customer nedFlanders;
+    
+    @Before
+    public void initData() {
+        homerSimpson = customer("Homer", "Simpson").registrationDate(2014, 03, 15).street("Evergreen Terrace").build(em);
+        margeSimpson = customer("Marge", "Simpson").registrationDate(2014, 03, 20).street("Evergreen Terrace").build(em);
+        bartSimpson = customer("Bart", "Simpson").registrationDate(2014, 03, 25).street("Evergreen Terrace").build(em);
+        moeSzyslak = customer("Moe", "Szyslak").registrationDate(2014, 03, 15).street("Unknown").build(em);
+        nedFlanders = customer("Ned", "Flanders").registrationDate(2014, 03, 25).street("Evergreen Terrace").build(em);
+    }
+    
+    @Test
+    public void shouldFilterWithBothSpecs() throws ParseException {
+        Like<Customer> streetWithEvergreen = new Like<>("address.street", "Evergreen");
+        DateBefore<Customer> registeredBefore21st = new DateBefore<>("registrationDate", new String[] {"2014-03-21"}, null);
+        
+        List<Customer> result = customerRepo.findAll(new Conjunction<>(streetWithEvergreen, registeredBefore21st));
+        
+        assertThat(result)
+            .hasSize(2)
+            .containsOnly(homerSimpson, margeSimpson);
+    }
+}
