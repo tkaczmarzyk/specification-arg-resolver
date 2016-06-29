963d3385cc3a5274ef4922f760b9586b7d346d08
Gerald Humphries <geraldhumphries@gmail.com>
2016-06-29 14:20:40 -0400

    added ability to pass boolean param to IsNull spec

---
+++ src/test/java/net/kaczmarzyk/spring/data/jpa/domain/NullTest.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2014-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.kaczmarzyk.spring.data.jpa.domain;
+
+import net.kaczmarzyk.spring.data.jpa.Customer;
+import net.kaczmarzyk.spring.data.jpa.IntegrationTestBase;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static net.kaczmarzyk.spring.data.jpa.CustomerBuilder.customer;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * @author Tomasz Kaczmarzyk
+ * @author Gerald Humphries
+ */
+public class NullTest extends IntegrationTestBase {
+
+	Customer bartSimpson;
+	Customer lisaSimpson;
+
+	@Before
+	public void initData() {
+		bartSimpson = customer("Bart", "Simpson").nickName("El Barto").street("Evergreen Terrace").build(em);
+		lisaSimpson = customer("Lisa", "Simpson").registrationDate(2014, 03, 20).street("Evergreen Terrace").build(em);
+	}
+
+	@Test
+	public void findsCustomersWithNullField() {
+		Null<Customer> spec = new Null<>("nickName", new String[]{"true"}, defaultConverter);
+
+		List<Customer> found = customerRepo.findAll(spec);
+
+		assertThat(found).hasSize(1).containsOnly(lisaSimpson);
+	}
+
+	@Test
+	public void findsCustomersWithNotNullField() {
+		Null<Customer> spec = new Null<>("nickName", new String[]{"false"}, defaultConverter);
+
+		List<Customer> found = customerRepo.findAll(spec);
+
+		assertThat(found).hasSize(1).containsOnly(bartSimpson);
+	}
+}
