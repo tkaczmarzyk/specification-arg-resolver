eb3747849604dbd30aca544ce4398d7c02c883ea
Matt S.Y. Ho <methodho@gmail.com>
2016-11-10 10:31:12 +0800

    Implemented spec: EndingWith, EndingWithIgnoreCase, StartingWith, StartingWithIgnoreCase

---
+++ src/test/java/net/kaczmarzyk/spring/data/jpa/domain/EndingWithIgnoreCaseWithTest.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2014-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package net.kaczmarzyk.spring.data.jpa.domain;
+
+import static net.kaczmarzyk.spring.data.jpa.CustomerBuilder.customer;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import net.kaczmarzyk.spring.data.jpa.Customer;
+import net.kaczmarzyk.spring.data.jpa.IntegrationTestBase;
+
+
+/**
+ * @author Matt S.Y. Ho
+ */
+public class EndingWithIgnoreCaseWithTest extends IntegrationTestBase {
+
+  Customer homerSimpson;
+  Customer margeSimpson;
+  Customer moeSzyslak;
+
+  @Before
+  public void initData() {
+    homerSimpson = customer("Homer", "Simpson").street("Evergreen Terrace").build(em);
+    margeSimpson = customer("Marge", "Simpson").street("Evergreen Terrace").build(em);
+    moeSzyslak = customer("Moe", "Szyslak").street("Unknown").build(em);
+  }
+
+  @Test
+  public void filtersByFirstLevelProperty() {
+    EndingWithIgnoreCase<Customer> lastNameSimpson = new EndingWithIgnoreCase<>("lastName", "SIMPSON");
+    List<Customer> result = customerRepo.findAll(lastNameSimpson);
+    assertThat(result).hasSize(2).containsOnly(homerSimpson, margeSimpson);
+
+    EndingWithIgnoreCase<Customer> firstNameWithO = new EndingWithIgnoreCase<>("firstName", "ER");
+    result = customerRepo.findAll(firstNameWithO);
+    assertThat(result).hasSize(1).containsOnly(homerSimpson);
+  }
+
+  @Test
+  public void filtersByNestedProperty() {
+    EndingWithIgnoreCase<Customer> streetWithEvergreen = new EndingWithIgnoreCase<>("address.street", "TERRACE");
+    List<Customer> result = customerRepo.findAll(streetWithEvergreen);
+    assertThat(result).hasSize(2).containsOnly(homerSimpson, margeSimpson);
+
+    EndingWithIgnoreCase<Customer> streetWithSpaceEvergreen =
+        new EndingWithIgnoreCase<>("address.street", "EVERGREEN");
+    result = customerRepo.findAll(streetWithSpaceEvergreen);
+    assertThat(result).hasSize(0);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void rejectsMissingArgument() {
+    new EndingWithIgnoreCase<>("path", new String[] {});
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void rejectsInvalidNumberOfArguments() {
+    new EndingWithIgnoreCase<>("path", new String[] {"a", "b"});
+  }
+}
---
+++ src/test/java/net/kaczmarzyk/spring/data/jpa/domain/StartingWithIgnoreCaseTest.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright 2014-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package net.kaczmarzyk.spring.data.jpa.domain;
+
+import static net.kaczmarzyk.spring.data.jpa.CustomerBuilder.customer;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import net.kaczmarzyk.spring.data.jpa.Customer;
+import net.kaczmarzyk.spring.data.jpa.IntegrationTestBase;
+
+/**
+ * 
+ * @author Matt S.Y. Ho
+ *
+ */
+public class StartingWithIgnoreCaseTest extends IntegrationTestBase {
+
+  Customer homerSimpson;
+  Customer margeSimpson;
+  Customer moeSzyslak;
+
+  @Before
+  public void initData() {
+    homerSimpson = customer("Homer", "Simpson").street("Evergreen Terrace").build(em);
+    margeSimpson = customer("Marge", "Simpson").street("Evergreen Terrace").build(em);
+    moeSzyslak = customer("Moe", "Szyslak").street("Unknown").build(em);
+  }
+
+  @Test
+  public void filtersByFirstLevelProperty() {
+    StartingWithIgnoreCase<Customer> lastNameSimpson = new StartingWithIgnoreCase<>("lastName", "SIMPSON");
+    List<Customer> result = customerRepo.findAll(lastNameSimpson);
+    assertThat(result).hasSize(2).containsOnly(homerSimpson, margeSimpson);
+
+    StartingWithIgnoreCase<Customer> firstNameWithO = new StartingWithIgnoreCase<>("firstName", "HO");
+    result = customerRepo.findAll(firstNameWithO);
+    assertThat(result).hasSize(1).containsOnly(homerSimpson);
+  }
+
+  @Test
+  public void filtersByNestedProperty() {
+    StartingWithIgnoreCase<Customer> streetWithEvergreen = new StartingWithIgnoreCase<>("address.street", "EVERGREEN");
+    List<Customer> result = customerRepo.findAll(streetWithEvergreen);
+    assertThat(result).hasSize(2).containsOnly(homerSimpson, margeSimpson);
+
+    StartingWithIgnoreCase<Customer> streetWithTerrace = new StartingWithIgnoreCase<>("address.street", "TERRACE");
+    result = customerRepo.findAll(streetWithTerrace);
+    assertThat(result).hasSize(0);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void rejectsMissingArgument() {
+    new StartingWithIgnoreCase<>("path", new String[] {});
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void rejectsInvalidNumberOfArguments() {
+    new StartingWithIgnoreCase<>("path", new String[] {"a", "b"});
+  }
+}
+
