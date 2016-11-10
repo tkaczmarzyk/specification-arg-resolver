ebadc8113d6d1c4c729617cdace39b30888843bc
Matt S.Y. Ho <methodho@gmail.com>
2016-11-10 10:31:12 +0800

    Implemented spec: EndingWith, EndingWithIgnoreCase, StartingWith, StartingWithIgnoreCase

---
+++ src/test/java/net/kaczmarzyk/spring/data/jpa/domain/StartingWithTest.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2014-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package net.kaczmarzyk.spring.data.jpa.domain;
+
+import static net.kaczmarzyk.spring.data.jpa.CustomerBuilder.customer;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import net.kaczmarzyk.spring.data.jpa.Customer;
+import net.kaczmarzyk.spring.data.jpa.IntegrationTestBase;
+
+
+/**
+ * @author Matt S.Y. Ho
+ */
+public class StartingWithTest extends IntegrationTestBase {
+
+  Customer homerSimpson;
+  Customer margeSimpson;
+  Customer moeSzyslak;
+
+  @Before
+  public void initData() {
+    homerSimpson = customer("Homer", "Simpson").street("Evergreen Terrace").build(em);
+    margeSimpson = customer("Marge", "Simpson").street("Evergreen Terrace").build(em);
+    moeSzyslak = customer("Moe", "Szyslak").street("Unknown").build(em);
+  }
+
+  @Test
+  public void filtersByFirstLevelProperty() {
+    StartingWith<Customer> lastNameSimpson = new StartingWith<>("lastName", "Simpson");
+    List<Customer> result = customerRepo.findAll(lastNameSimpson);
+    assertThat(result).hasSize(2).containsOnly(homerSimpson, margeSimpson);
+
+    StartingWith<Customer> firstNameWithO = new StartingWith<>("firstName", "Ho");
+    result = customerRepo.findAll(firstNameWithO);
+    assertThat(result).hasSize(1).containsOnly(homerSimpson);
+  }
+
+  @Test
+  public void filtersByNestedProperty() {
+    StartingWith<Customer> streetWithEvergreen = new StartingWith<>("address.street", "Evergreen");
+    List<Customer> result = customerRepo.findAll(streetWithEvergreen);
+    assertThat(result).hasSize(2).containsOnly(homerSimpson, margeSimpson);
+
+    StartingWith<Customer> streetWithTerrace = new StartingWith<>("address.street", "Terrace");
+    result = customerRepo.findAll(streetWithTerrace);
+    assertThat(result).hasSize(0);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void rejectsMissingArgument() {
+    new StartingWith<>("path", new String[] {});
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void rejectsInvalidNumberOfArguments() {
+    new StartingWith<>("path", new String[] {"a", "b"});
+  }
+}
