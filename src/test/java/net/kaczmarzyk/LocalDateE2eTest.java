7ff0b0bcefd1858679c0a72b394aea0e5b084686
Tomasz Kaczmarzyk <tomasz+github@kaczmarzyk.net>
2019-02-05 19:52:10 -0100

    more tests and better assertions for LocalDate support

--- src/test/java/net/kaczmarzyk/LocalDateE2eTest.java
+++ src/test/java/net/kaczmarzyk/LocalDateE2eTest.java
@@ -15,12 +15,10 @@
  */
 package net.kaczmarzyk;
 
-import net.kaczmarzyk.spring.data.jpa.Customer;
-import net.kaczmarzyk.spring.data.jpa.CustomerRepository;
-import net.kaczmarzyk.spring.data.jpa.domain.DateBetween;
-import net.kaczmarzyk.spring.data.jpa.domain.GreaterThan;
-import net.kaczmarzyk.spring.data.jpa.domain.LessThan;
-import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
@@ -29,9 +27,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import net.kaczmarzyk.spring.data.jpa.Customer;
+import net.kaczmarzyk.spring.data.jpa.CustomerRepository;
+import net.kaczmarzyk.spring.data.jpa.domain.GreaterThan;
+import net.kaczmarzyk.spring.data.jpa.domain.LessThan;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
+
 
 public class LocalDateE2eTest extends E2eTestBase {
     @Controller
@@ -42,37 +43,77 @@ public class LocalDateE2eTest extends E2eTestBase {
 
         @RequestMapping(value = "/customers", params = "birthDateBefore")
         @ResponseBody
-        public Object findCustomersRegisteredBefore(
-                @Spec(path="birthDate", params="birthDateBefore", config="dd-MM-yyyy", spec= LessThan.class) Specification<Customer> spec) {
+        public Object findCustomersBornBefore_defaultDateFormat(
+                @Spec(path="birthDate", params="birthDateBefore", spec= LessThan.class) Specification<Customer> spec) {
+
+            return customerRepo.findAll(spec);
+        }
+        
+        @RequestMapping(value = "/customers", params = "birthDateBefore_customFormat")
+        @ResponseBody
+        public Object findCustomersBornBefore_customDateFormat(
+                @Spec(path="birthDate", params="birthDateBefore_customFormat", config="dd/MM/yyyy", spec= LessThan.class) Specification<Customer> spec) {
 
             return customerRepo.findAll(spec);
         }
 
         @RequestMapping(value = "/customers", params = "birthDateAfter")
         @ResponseBody
-        public Object findCustomersRegisteredAfter(
+        public Object findCustomersBornAfter_defaultDateFormat(
                 @Spec(path="birthDate", params="birthDateAfter", spec= GreaterThan.class) Specification<Customer> spec) {
+
+            return customerRepo.findAll(spec);
+        }
+        
+        @RequestMapping(value = "/customers", params = "birthDateAfter_customFormat")
+        @ResponseBody
+        public Object findCustomersBornAfter_customDateFormat(
+                @Spec(path="birthDate", params="birthDateAfter_customFormat", config="dd/MM/yyyy", spec= GreaterThan.class) Specification<Customer> spec) {
 
             return customerRepo.findAll(spec);
         }
 
     }
-
+    
     @Test
     public void findsByDateBeforeWithCustomDateFormat() throws Exception {
         mockMvc.perform(get("/customers")
-                                .param("birthDateBefore", "16-03-1996")
+                                .param("birthDateBefore_customFormat", "12/07/1972")
                                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isArray())
                 .andExpect(jsonPath("$[0].firstName").value("Homer"))
-                .andExpect(jsonPath("$[1].firstName").value("Marge"));
+                .andExpect(jsonPath("$[1].firstName").value("Moe"))
+                .andExpect(jsonPath("$[2]").doesNotExist());
     }
 
     @Test
-    public void findsByDateAfter() throws Exception {
+    public void findsByDateBeforeWithDefaultDateFormat() throws Exception {
+        mockMvc.perform(get("/customers")
+                                .param("birthDateBefore", "1972-07-12")
+                                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0].firstName").value("Homer"))
+                .andExpect(jsonPath("$[1].firstName").value("Moe"))
+                .andExpect(jsonPath("$[2]").doesNotExist());
+    }
+
+    @Test
+    public void findsByDateAfterWithDefaultDateFormat() throws Exception {
         mockMvc.perform(get("/customers")
                                 .param("birthDateAfter", "1996-03-26")
+                                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0].firstName").value("Maggie"))
+                .andExpect(jsonPath("$[2]").doesNotExist());
+    }
+    
+    @Test
+    public void findsByDateAfterWithCustomDateFormat() throws Exception {
+        mockMvc.perform(get("/customers")
+                                .param("birthDateAfter_customFormat", "26/03/1996")
                                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isArray())
