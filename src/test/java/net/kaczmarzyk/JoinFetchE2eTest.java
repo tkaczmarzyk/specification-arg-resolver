495d656e950551a9339530288d56e976785df326
Piotr Chłystek <41192874+pchlystek@users.noreply.github.com>
2020-05-11 19:25:28 +0200

    [SAR-62] Path evaluation fixes (#69)
    * Fixed count query not working since it was referencing wrong root. Add tests for pagination with joins
    
    Co-authored-by: Tom Kedem <tomkedem@gmail.com>
    Co-authored-by: Tomasz Kaczmarzyk <tomasz+github@kaczmarzyk.net>

--- src/test/java/net/kaczmarzyk/JoinFetchE2eTest.java
+++ src/test/java/net/kaczmarzyk/JoinFetchE2eTest.java
@@ -18,15 +18,18 @@ package net.kaczmarzyk;
 import net.kaczmarzyk.spring.data.jpa.Customer;
 import net.kaczmarzyk.spring.data.jpa.CustomerRepository;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
+import net.kaczmarzyk.spring.data.jpa.domain.Like;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.JoinFetch;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -40,12 +43,22 @@ public class JoinFetchE2eTest extends E2eTestBase {
 		private CustomerRepository customerRepository;
 
 		@RequestMapping("/join-fetch/customers")
-		public Object findByName(
+		public Object findByFirstName(
 
 				@JoinFetch(paths = "badges")
 				@Spec(path = "firstName", spec = Equal.class) Specification<Customer> spec) {
 
 			return customerRepository.findAll(spec);
+		}
+
+		@RequestMapping("/join-fetch-pageable/customers")
+		public Object findByLastNameWithPagination(
+
+				@JoinFetch(paths = "badges")
+				@Spec(path = "lastName", spec = Equal.class) Specification<Customer> spec,
+				Pageable pageable) {
+
+			return customerRepository.findAll(spec, pageable);
 		}
 
 	}
@@ -59,6 +72,23 @@ public class JoinFetchE2eTest extends E2eTestBase {
 			.andExpect(jsonPath("$").isArray())
 			.andExpect(jsonPath("$[0].firstName").value("Homer"))
 			.andExpect(jsonPath("$[1]").doesNotExist());
+	}
+
+	@Test
+	public void findsByLastNameWithPagination() throws Exception {
+		mockMvc.perform(get("/join-fetch-pageable/customers")
+				.param("lastName", "Simpson")
+				.param("page", "0")
+				.param("size", "1")
+				.param("sort", "id")
+				.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.content").isArray())
+			.andExpect(jsonPath("$.content", hasSize(1)))
+			.andExpect(jsonPath("$.content[0].firstName").value("Homer"))
+			.andExpect(jsonPath("$.totalPages").value(5))
+			.andExpect(jsonPath("$.totalElements").value(5))
+			.andExpect(jsonPath("$.size").value(1));
 	}
 
 }
