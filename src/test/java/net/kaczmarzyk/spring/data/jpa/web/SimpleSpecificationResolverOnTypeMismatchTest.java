bc9ea5ce06afceb2fa39815ae2422c6bafd701e8
Jakub Radlica <jakub.radlica@gmail.com>
2020-06-01 16:51:31 +0200

    Improved formatting

--- src/test/java/net/kaczmarzyk/spring/data/jpa/web/SimpleSpecificationResolverOnTypeMismatchTest.java
+++ src/test/java/net/kaczmarzyk/spring/data/jpa/web/SimpleSpecificationResolverOnTypeMismatchTest.java
@@ -15,61 +15,60 @@
  */
 package net.kaczmarzyk.spring.data.jpa.web;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-
-import org.junit.Test;
-import org.springframework.core.MethodParameter;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.web.context.request.NativeWebRequest;
-
 import net.kaczmarzyk.spring.data.jpa.domain.EmptyResultOnTypeMismatch;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.WithoutTypeConversion;
 import net.kaczmarzyk.spring.data.jpa.utils.QueryContext;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.OnTypeMismatch;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
+import org.junit.Test;
+import org.springframework.core.MethodParameter;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.web.context.request.NativeWebRequest;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 public class SimpleSpecificationResolverOnTypeMismatchTest extends ResolverTestBase {
 
-    SimpleSpecificationResolver resolver = new SimpleSpecificationResolver();
+	SimpleSpecificationResolver resolver = new SimpleSpecificationResolver();
 
 	@Test
-    public void usesEmptyResultSpecWrapperWhenSpecified() throws Exception {
-    	MethodParameter param = MethodParameter.forExecutable(testMethod("testMethodWithOnTypeMismatchConfig"), 0);
-        NativeWebRequest req = mock(NativeWebRequest.class);
-        when(req.getParameterValues("thePath")).thenReturn(new String[] { "theValue" });
-
-		WebRequestProcessingContext ctx = new WebRequestProcessingContext(param, req);
-
-		Specification<?> resolved = resolver.buildSpecification(ctx, param.getParameterAnnotation(Spec.class));
-
-        assertThat(resolved)
-        	.isInstanceOf(EmptyResultOnTypeMismatch.class);
-
-        assertThat(((EmptyResultOnTypeMismatch<?>) resolved).getWrappedSpec()).isEqualTo(new Equal<>(ctx.queryContext(), "thePath", new String [] { "theValue" }, defaultConverter));
-    }
-
-	@Test
-	public void doesNotWrapWithEmptyResultSpecWhenSpecificationDoesntRequireDataConversion() throws Exception {
-		MethodParameter param = MethodParameter.forExecutable(testMethod("testMethodWithSpecWithoutDataConversion"), 0);
-        NativeWebRequest req = mock(NativeWebRequest.class);
-        when(req.getParameterValues("thePath")).thenReturn(new String[] { "theValue" });
+	public void usesEmptyResultSpecWrapperWhenSpecified() throws Exception {
+		MethodParameter param = MethodParameter.forExecutable(testMethod("testMethodWithOnTypeMismatchConfig"), 0);
+		NativeWebRequest req = mock(NativeWebRequest.class);
+		when(req.getParameterValues("thePath")).thenReturn(new String[]{ "theValue" });
 
 		WebRequestProcessingContext ctx = new WebRequestProcessingContext(param, req);
 
 		Specification<?> resolved = resolver.buildSpecification(ctx, param.getParameterAnnotation(Spec.class));
 
 		assertThat(resolved)
-        	.isNotInstanceOf(EmptyResultOnTypeMismatch.class)
-        	.isInstanceOf(SpecWithoutDataConversion.class);
+				.isInstanceOf(EmptyResultOnTypeMismatch.class);
+
+		assertThat(((EmptyResultOnTypeMismatch<?>) resolved).getWrappedSpec()).isEqualTo(new Equal<>(ctx.queryContext(), "thePath", new String[]{ "theValue" }, defaultConverter));
+	}
+
+	@Test
+	public void doesNotWrapWithEmptyResultSpecWhenSpecificationDoesntRequireDataConversion() throws Exception {
+		MethodParameter param = MethodParameter.forExecutable(testMethod("testMethodWithSpecWithoutDataConversion"), 0);
+		NativeWebRequest req = mock(NativeWebRequest.class);
+		when(req.getParameterValues("thePath")).thenReturn(new String[]{ "theValue" });
+
+		WebRequestProcessingContext ctx = new WebRequestProcessingContext(param, req);
+
+		Specification<?> resolved = resolver.buildSpecification(ctx, param.getParameterAnnotation(Spec.class));
+
+		assertThat(resolved)
+				.isNotInstanceOf(EmptyResultOnTypeMismatch.class)
+				.isInstanceOf(SpecWithoutDataConversion.class);
 	}
 
 	public static class SpecWithoutDataConversion implements Specification<Object>, WithoutTypeConversion {
@@ -83,12 +82,16 @@ public class SimpleSpecificationResolverOnTypeMismatchTest extends ResolverTestB
 		}
 	}
 
-    public static class TestController {
+	public static class TestController {
 
-    	public void testMethodWithSpecWithoutDataConversion(@Spec(path = "thePath", spec = SpecWithoutDataConversion.class, onTypeMismatch = OnTypeMismatch.EMPTY_RESULT) Specification<Object> spec) {};
+		public void testMethodWithSpecWithoutDataConversion(@Spec(path = "thePath", spec = SpecWithoutDataConversion.class, onTypeMismatch = OnTypeMismatch.EMPTY_RESULT) Specification<Object> spec) {
+		}
 
-        public void testMethodWithOnTypeMismatchConfig(@Spec(path = "thePath", spec = Equal.class, onTypeMismatch=OnTypeMismatch.EMPTY_RESULT) Specification<Object> spec) {}
-    }
+		;
+
+		public void testMethodWithOnTypeMismatchConfig(@Spec(path = "thePath", spec = Equal.class, onTypeMismatch = OnTypeMismatch.EMPTY_RESULT) Specification<Object> spec) {
+		}
+	}
 
 	@Override
 	protected Class<?> controllerClass() {
