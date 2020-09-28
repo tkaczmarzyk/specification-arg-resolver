a39d0eda19e11cfe4b6a080ea8158ad7dbaa9936
Jakub Radlica <jakub.radlica@gmail.com>
2020-09-28 17:44:00 +0200

    Improvements after CR

--- src/main/java/net/kaczmarzyk/spring/data/jpa/domain/PathSpecification.java
+++ src/main/java/net/kaczmarzyk/spring/data/jpa/domain/PathSpecification.java
@@ -59,7 +59,15 @@ public abstract class PathSpecification<T> implements Specification<T> {
 	private Path<T> getEvaluatedPath(String field, Root<T> root) {
 		Path<T> evaluated = (Path<T>) queryContext.getEvaluated(field, root);
 
-		// evaluated could be null if join with given alias has been not defined
+		/**
+		 * if join with given alias is not defined, evaluated variable has null value in this place.
+		 *
+		 * Example spec:
+		 *     @JoinFetch(paths = "orders", alias = "o")
+		 *     @Spec(path = "o.itemName", params = "itemName", spec = Equal.class)
+		 *     interface CustomersWithOrderedItemSpecification { }
+ 		 */
+
 		if(evaluated == null) {
 			return (Path<T>) queryContext.getEvaluatedJoinFetch(field);
 		}
