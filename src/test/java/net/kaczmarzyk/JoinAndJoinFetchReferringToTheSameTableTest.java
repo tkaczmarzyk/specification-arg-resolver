3fa7da117a775c01eeac7a71709121b485b95b39
jradlica <jakub.radlica@gmail.com>
2022-11-17 13:48:36 +0100

    Rebased to master

--- src/test/java/net/kaczmarzyk/JoinAndJoinFetchReferringToTheSameTableTest.java
+++ src/test/java/net/kaczmarzyk/JoinAndJoinFetchReferringToTheSameTableTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014-2020 the original author or authors.
+ * Copyright 2014-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,6 +48,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+/**
+ * @author Jakub Radlica
+ */
 public class JoinAndJoinFetchReferringToTheSameTableTest extends E2eTestBase {
 
 	@Controller
--- src/test/java/net/kaczmarzyk/spring/data/jpa/CustomerDto.java
+++ src/test/java/net/kaczmarzyk/spring/data/jpa/CustomerDto.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014-2020 the original author or authors.
+ * Copyright 2014-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +18,9 @@ package net.kaczmarzyk.spring.data.jpa;
 import java.util.Collection;
 import java.util.Objects;
 
+/**
+ * @author Jakub Radlica
+ */
 public class CustomerDto {
 	private String firstName;
 	private String tagOfFirstCustomerItem;
