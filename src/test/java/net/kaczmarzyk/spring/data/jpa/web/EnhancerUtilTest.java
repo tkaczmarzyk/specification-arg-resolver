4fe62abb6a94f14a0dbc12512e85f3f7010228a2
klesniak1 <klesniak@tascent.com>
2022-12-13 15:45:23 +0100

    improved EnhancerUtil tests

--- src/test/java/net/kaczmarzyk/spring/data/jpa/web/EnhancerUtilTest.java
+++ src/test/java/net/kaczmarzyk/spring/data/jpa/web/EnhancerUtilTest.java
@@ -19,6 +19,8 @@ import net.kaczmarzyk.spring.data.jpa.domain.Conjunction;
 import net.kaczmarzyk.spring.data.jpa.domain.Disjunction;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.utils.SimpleSpecificationGenerator;
+import net.kaczmarzyk.spring.data.jpa.utils.SpecificationBuilder;
+import net.kaczmarzyk.utils.ReflectionUtils;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.springframework.data.jpa.domain.Specification;
@@ -168,6 +170,15 @@ public class EnhancerUtilTest {
 		CustomSpecInterface secondSpecInterface = EnhancerUtil.wrapWithIfaceImplementation(SpecExtendingCustomSpecInterface.class, secondSpec);
 
 		Assertions.assertThat(customSpecInterface).isNotEqualTo(secondSpecInterface);
+	}
+
+	@Test
+	public void equalsContract_enhancedInterfaceIsNotEqualToNull() {
+		Specification<Object> firstSpec = testSpecification(Equal.class);
+
+		CustomSpecInterface customSpecInterface = EnhancerUtil.wrapWithIfaceImplementation(CustomSpecInterface.class, firstSpec);
+
+		Assertions.assertThat(customSpecInterface).isNotEqualTo(null);
 	}
 
 	private List<Specification<Object>> testSimpleSpecifications() {
