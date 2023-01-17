8fc07ad5b3f538fbc7c635d0c0a3c25f9632fc84
Hubert <hgotfryd@tascent.com>
2023-01-17 12:27:37 +0100

    Registered new specifications in `SpecificationArgumentResolverHintRegistrar`

--- src/test/java/net/kaczmarzyk/spring/data/jpa/nativeimage/SpecificationArgumentResolverHintRegistrarTest.java
+++ src/test/java/net/kaczmarzyk/spring/data/jpa/nativeimage/SpecificationArgumentResolverHintRegistrarTest.java
@@ -49,7 +49,7 @@ public class SpecificationArgumentResolverHintRegistrarTest {
 
         //additional check that each specification type has been discovered
         assertThat(specificationClasses)
-                .hasSize(30);
+                .hasSize(41);
 
         SpecificationArgumentResolverHintRegistrar hintRegistrar = new SpecificationArgumentResolverHintRegistrar();
 
@@ -58,7 +58,7 @@ public class SpecificationArgumentResolverHintRegistrarTest {
 
         //then
         assertThat(runtimeHints.reflection().typeHints())
-                        .hasSize(30);
+                        .hasSize(41);
 
         specificationClasses.forEach(domainClass -> {
             TypeHint classReflectionHints = runtimeHints.reflection().getTypeHint(domainClass.loadClass());
